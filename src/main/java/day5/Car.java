528189909ce1d2e2fcec68f38775ac1d407dbc08
Olga Raukuts <raukuts.olga@gmail.com>
2023-10-03 10:30:14 +0200

    Revert "test"
    This reverts commit 19687d2e3ddb5330d4d52d4391692aaa2c423525.

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class Car {
+    private String make;
+    private String color;
+    private int year;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,11 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car car = new Car();
+        car.setMake("Porsche");
+        car.setColor("Green");
+        car.setYear(2020);
+
+        System.out.println("the car make " + car.getMake() + " of " + car.getColor() +" color is available." + "manufactured " + car.getYear());
     }
 }
