3cc94f2a7d73ae8a301d191ed06497425b9e6bab
Olga Raukuts <raukuts.olga@gmail.com>
2023-10-08 11:31:05 +0200

    complete day7 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        String numbers = " ";
+
+        for(int i = 0; i <= 20000; i++){
+        numbers = numbers + i + " ";
+            System.out.println(numbers);
+        }
+
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы: " + (stopTime - startTime));
+
+        System.out.println(" ============================ ");
+        long startTime1 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder(numbers);
+        System.out.println(sb.toString());
+        long stopTime1 = System.currentTimeMillis();
+        System.out.println("Длительность работы: " + (stopTime1 - startTime1));
 
     }
+
+
 }
