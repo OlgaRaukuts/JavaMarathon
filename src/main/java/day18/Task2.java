c965d17b86c1a72078ecaca22dcb08f01e3d72d9
Olga Raukuts <raukuts.olga@gmail.com>
2023-10-20 11:08:23 +0200

    complete day18 tasks

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -3,5 +3,16 @@ package day18;
 public class Task2 {
     public static void main(String[] args) {
 
+        System.out.println(recursionNum(357774267));
+    }
+
+    public static int recursionNum(int number){
+        if (number == 0)
+            return 0;
+
+        if (number % 10  ==7 )
+            return 1 + recursionNum(number/10);
+        else
+            return  recursionNum(number/10);
     }
 }
